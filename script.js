520e9ae267b6ca043242a6d5e926bd9ae1bdb725
Pratheek Senevirathne <pratheeksenevirathne@gmail.com>
2021-05-24 11:30:34 +0530

    navigation refactored

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 "use strict";
 
-window.addEventListener("resize", () => {
-  console.log("💩");
-  if (window.innerWidth < 600) {
-    navList.classList.remove("display-flex");
-    navList.classList.add("display-none");
-  } else {
-    navList.classList.remove("display-none");
-    navList.classList.add("display-flex");
-  }
-});
+// window.addEventListener("resize", () => {
+//   console.log("💩");
+//   if (window.innerWidth < 600) {
+//     navList.classList.remove("display-flex");
+//     navList.classList.add("display-none");
+//   } else {
+//     navList.classList.remove("display-none");
+//     navList.classList.add("display-flex");
+//   }
+// });
 
 const hamburger = document.querySelector(".hamburger");
-const navList = document.querySelector("nav ul");
-const navLinks = document.querySelectorAll("nav li a");
+const navList = document.querySelector(".nav-link-wrapper");
+const navLinks = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", () => {
-  if (navList.classList.contains("display-none")) {
-    navList.classList.remove("display-none");
-    navList.classList.add("display-flex");
+  if (navList.classList.contains("hide-nav")) {
+    navList.classList.remove("hide-nav");
+    navList.classList.add("show-nav");
   } else {
-    navList.classList.remove("display-flex");
-    navList.classList.add("display-none");
+    navList.classList.remove("show-nav");
+    navList.classList.add("hide-nav");
   }
 });
 
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", () => {
-    navList.classList.remove("display-flex");
-    navList.classList.add("display-none");
+    navList.classList.remove("show-nav");
+    navList.classList.add("hide-nav");
   });
 });
