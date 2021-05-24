b6ba758fb14ffdedba5735bebeeecdc7c66a2175
Pratheek Senevirathne <pratheeksenevirathne@gmail.com>
2021-05-24 21:50:34 +0530

    Contact section css and js

--- script.js
+++ script.js
@@ -31,3 +31,31 @@ navLinks.forEach((navLink) => {
     navList.classList.add("hide-nav");
   });
 });
+
+const contactItems = document.querySelectorAll(".contact-item");
+
+contactItems.forEach((contactItem) => {
+  contactItem.addEventListener("click", (event) => {
+    const contactChildren = contactItem.children;
+    const link = contactChildren[1].href;
+    const target = contactChildren[1].target;
+    console.log(link, target);
+    if (!target) {
+      window.open(link, "_self");
+    } else {
+      window.open(link, target);
+    }
+  });
+
+  contactItem.addEventListener("mouseenter", (event) => {
+    const contactChildren = contactItem.children;
+    contactChildren[0].style.color = "rgb(64, 139, 243)";
+    contactChildren[1].style.color = "rgb(64, 139, 243)";
+  });
+
+  contactItem.addEventListener("mouseleave", (event) => {
+    const contactChildren = contactItem.children;
+    contactChildren[0].style.color = "rgb(10, 48, 99)";
+    contactChildren[1].style.color = "rgb(10, 48, 99)";
+  });
+});
